bf825cc95924d0181b329d8240c3d5d1c7d00225
CarlosDevEGJ <carlosegj.developer@gmail.com>
2026-07-16 14:48:36 -0400

    feature> creado el controlador de clientes para consumir en la api

---
+++ Api/Controllers/ClienteController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Module.Customers.Application.Interfaces;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClienteController : Controller
+    {
+        private readonly IClienteService _clienteService;
+
+        public ClienteController(IClienteService clienteService)
+        {
+            _clienteService = clienteService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllClientes()
+        {
+            var clientes = await _clienteService.GetAllAsync();
+            return Ok(clientes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetClienteById(int id)
+        {
+            var cliente = await _clienteService.GetByIdAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return Ok(cliente);
+        }
+
+
+
+    }
+}
