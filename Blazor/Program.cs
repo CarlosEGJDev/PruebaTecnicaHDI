ae5592a61a2afa0cd782e71b8bbb66c71111fa4c
CarlosDevEGJ <carlosegj.developer@gmail.com>
2026-07-16 15:49:10 -0400

    feature agregada la funcionalidad para ver clientes de forma temporal.

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -5,10 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
-//builder.Services.AddHttpClient("CustomerAPI", client =>
-//{
-//    client.BaseAddress = new Uri("https://localhost:7221/api/");
-//});
+builder.Services.AddHttpClient("CustomerAPI", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7221/");
+});
 
 var app = builder.Build();
 
